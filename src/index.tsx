17e1a5edf053224adad1ee16733f6b80b64b18a2
mecirmartin <mecir.martin@gmail.com>
2021-03-05 10:34:06 +0100

    changed file extensions to .tsx, removed eslint

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+
+// @ts-ignore
+if (!window.__skip_render) {
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
+}
+
+serviceWorker.unregister();
