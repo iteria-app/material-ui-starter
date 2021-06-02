a50c1fa18f8f94a330e845c71c1fd76962a2fd84
mecirmartin <55835379+mecirmartin@users.noreply.github.com>
2021-06-02 14:09:36 +0200

    Delete en.ts

--- src/compiled-lang/en.ts
+++
@@ -1,9 +0,0 @@
- const en = {
-    "customers": "CUSTOMERS",
-    "total.customers": "TOTAL CUSTOMERS",
-    "total.profit": "TOTAL PROFIT",
-    "budget": "BUDGET",
-    "tasks.progress": "TASKS PROGRESS"
-}
-
-export default en
