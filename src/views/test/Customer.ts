7a7adbe9673ce01c4a4a3c824e1a2ae66eafa176
mat-app <matejchatrnuch@gmail.com>
2021-06-09 10:34:05 +0200

    - add customer detail

---
+++ src/views/test/Customer.ts
@@ -0,0 +1,9 @@
+export interface Customer {
+    avatarUrl: string;
+    createdAt: number;
+    email: string;
+    name: string;
+    phone: string;
+    updatedAt: number;
+    test2: number;
+  }
--- src/views/test/GeneratedCustomersTable.tsx
+++ src/views/test/GeneratedCustomersTable.tsx
@@ -1,16 +1,13 @@
 import { useIntl,FormattedMessage } from "react-intl";
     import { GridColParams,DataGrid } from "@material-ui/data-grid";
-    import * as React from "react";
-    export default function CustomerIndex({ customers }) {
+    export default function CustomerTable({ customers }) {
         const intl = useIntl();
         const columns = [
-            { field: "avatarUrl", width: 180, type: "string", valueFormatter: ({ value }) => value, renderHeader: (params: GridColParams) => (<FormattedMessage id="Customer.avatarUrl" defaultMessage="avatarUrl"/>) },
-            { field: "createdAt", width: 180, type: "date", valueFormatter: ({ value }) => intl.formatDate(value), renderHeader: (params: GridColParams) => (<FormattedMessage id="Customer.createdAt" defaultMessage="createdAt"/>) },
-            { field: "email", width: 180, type: "string", valueFormatter: ({ value }) => value, renderHeader: (params: GridColParams) => (<FormattedMessage id="Customer.email" defaultMessage="email"/>) },
-            { field: "id", width: 180, type: "string", valueFormatter: ({ value }) => value, renderHeader: (params: GridColParams) => (<FormattedMessage id="Customer.id" defaultMessage="id"/>) },
-            { field: "name", width: 180, type: "string", valueFormatter: ({ value }) => value, renderHeader: (params: GridColParams) => (<FormattedMessage id="Customer.name" defaultMessage="name"/>) },
-            { field: "phone", width: 180, type: "string", valueFormatter: ({ value }) => value, renderHeader: (params: GridColParams) => (<FormattedMessage id="Customer.phone" defaultMessage="phone"/>) },
-            { field: "updatedAt", width: 180, type: "date", valueFormatter: ({ value }) => intl.formatDate(value), renderHeader: (params: GridColParams) => (<FormattedMessage id="Customer.updatedAt" defaultMessage="updatedAt"/>) }
+            { field: "avatarUrl", flex: 1, type: "string", valueFormatter: ({ value }) => value, renderHeader: (params: GridColParams) => (<FormattedMessage id="Customer" defaultMessage="avatarUrl"/>) },
+            { field: "createdAt", flex: 1, type: "date", valueFormatter: ({ value }) => intl.formatDate(value), renderHeader: (params: GridColParams) => (<FormattedMessage id="Customer" defaultMessage="createdAt"/>) },
+            { field: "email", flex: 1, type: "string", valueFormatter: ({ value }) => value, renderHeader: (params: GridColParams) => (<FormattedMessage id="Customer" defaultMessage="email"/>) },
+            { field: "id", flex: 1, type: "string", valueFormatter: ({ value }) => value, renderHeader: (params: GridColParams) => (<FormattedMessage id="Customer" defaultMessage="id"/>) },
+            { field: "name", flex: 1, type: "string", valueFormatter: ({ value }) => value, renderHeader: (params: GridColParams) => (<FormattedMessage id="Customer" defaultMessage="name"/>) },
         ];
-        return (<div style={{ height: "400px", width: "100%" }}>here should be generated customer table</div>);
+        return (<div style={{ height: "400px", width: "100%" }}><DataGrid columns={columns} rows={customers}/></div>);
     }
