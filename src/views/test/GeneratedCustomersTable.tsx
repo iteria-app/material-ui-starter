2e27fa16ef80abcb95e17563b254e978e11d7ec8
Martin Svoren <msvoro@gmail.com>
2021-07-06 17:21:02 +0200

    filtered refaktoring

--- src/views/test/GeneratedCustomersTable.tsx
+++ src/views/test/GeneratedCustomersTable.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { useIntl, FormattedMessage } from "react-intl";
 import { GridColParams, DataGrid } from "@material-ui/data-grid";
 import { useNavigate } from 'react-router-dom'
-import { sortCustomers, unsortCustomers} from './OperationsCustomers'
+import { sortCustomers, unsortCustomers } from './OperationsCustomers'
 
 export default function CustomerTable({ customers, onOrderCustomer, onPageChangeCustomer, page, limit, limitCustomer, onFilterCustomer, totalCustomer }) {
     let navigate = useNavigate();
-    
+
     const handleSortCustomers = (sort) => {
         console.log(sort?.sortModel, 'sort?.sortModel');
         if (sort?.sortModel?.length > 0) {
@@ -16,7 +16,7 @@ export default function CustomerTable({ customers, onOrderCustomer, onPageChange
         }
     };
 
-    console.log(totalCustomer,'totalCustomer'); 
+    console.log(totalCustomer, 'totalCustomer');
 
     const handlePage = (page) => {
         console.log(page.page, 'page.page')
@@ -25,62 +25,89 @@ export default function CustomerTable({ customers, onOrderCustomer, onPageChange
 
     const handlePageSize = (pageSize) => {
         limitCustomer(pageSize?.pageSize)
-        console.log(pageSize,'params'); 
+        console.log(pageSize, 'params');
     }
 
     const handleFilter = React.useCallback((filter) => {
         // const filterValue = filter?.filterModel?.items[0]?.value
-        console.log(filter,'filter'); 
-        
+        console.log(filter, 'filter');
+
         // const filterArr1 = filter?.filterModel?.items
-        interface filterArr {
+        interface filteredQuery {
             filterCategory?: {}
-        } 
-        const filteredArrForGraphQl: filterArr  = {}
+        }
+        const filteredQueryForGraphQl: filteredQuery = {}
 
-        for (let i = 0; i < filteredArrayFromUi(filter).length; i++)   {
-            const filterCategory = filteredArrayFromUi(filter)[i].columnField
-            const filterOperator = filteredArrayFromUi(filter)[i].operatorValue
-            const filterValue = filteredArrayFromUi(filter)[i].value
+        for (let i = 0; i < queryFromDataGrid(filter).length; i++) {
+            const filterCategory = queryFromDataGrid(filter)[i].columnField
+            const filterOperator = queryFromDataGrid(filter)[i].operatorValue
+            const filterValue = queryFromDataGrid(filter)[i].value
 
-            if(filterOperator === 'contains'){
-                filteredArrForGraphQl[filterCategory] = {_ilike: "%" + filterValue + "%"}
-            }else if(filterOperator === 'endsWith'){
-                filteredArrForGraphQl[filterCategory] = {_ilike: "%" + filterValue}
+            if (filterOperator === 'contains') {
+                filteredQueryForGraphQl[filterCategory] = { _ilike: "%" + filterValue + "%" }
+            } else if (filterOperator === 'endsWith') {
+                filteredQueryForGraphQl[filterCategory] = { _ilike: "%" + filterValue }
             }
-            else if(filterOperator === 'startsWith'){
-                filteredArrForGraphQl[filterCategory] = {_ilike: filterValue + "%"}
+            else if (filterOperator === 'startsWith') {
+                filteredQueryForGraphQl[filterCategory] = { _ilike: filterValue + "%" }
             }
-            else if(filterOperator === 'equals'){
-                filteredArrForGraphQl[filterCategory] = {_eq: filterValue}
+            else if (filterOperator === 'equals') {
+                filteredQueryForGraphQl[filterCategory] = { _eq: filterValue }
             }
-    
-            console.log(filteredArrForGraphQl,'filteredArrForGraphQl'); 
+
+            console.log(filteredQueryForGraphQl, 'filteredQueryForGraphQl');
         }
 
-        if (filteredValueFromUi(filter)) {
-            onFilterCustomer(filteredArrForGraphQl);
-            onPageChangeCustomer(1)
-            console.log(filteredValueFromUi(filter), 'filterValue');
-        } else {
-            onFilterCustomer({
-                name: {
-                    _ilike: "%%"
-                }
-              })
-            onPageChangeCustomer(1)
-        }
+        setCurrentPageToOne()
+        sendQueryFilterToGraphQl(filter, filteredQueryForGraphQl)
+        // if (filteredValueFromDataGrid(filter)) {
+        //     // onFilterCustomer(filteredQueryForGraphQl);
+        //     // setCurrentPageToOne()
+        //     setFilteredQueryToGraphQl(filteredQueryForGraphQl)
+        //     console.log(filteredValueFromDataGrid(filter), 'filterValue');
+        // } else {
+        //     setDefaultFilteredQueryToGraphQl()
+        // }
     }, [onFilterCustomer]);
 
-    const filteredValueFromUi = (filter) => {
+    const filteredValueFromDataGrid = (filter) => {
         return filter?.filterModel?.items[0]?.value
     }
 
-    const filteredArrayFromUi = (filter) => {
+    const queryFromDataGrid = (filter) => {
         return filter?.filterModel?.items
     }
 
-    console.log(page,'page'); 
+    const setFilteredQueryToGraphQl = (filteredQueryForGraphQl) => {
+        onFilterCustomer(filteredQueryForGraphQl);
+    }
+
+    const setDefaultFilteredQueryToGraphQl = () => {
+        onFilterCustomer({
+            name: {
+                _ilike: "%%"
+            }
+        })
+    }
+
+    const sendQueryFilterToGraphQl = (filter, filteredQueryForGraphQl) => {
+        if (filteredValueFromDataGrid(filter)) {
+            // onFilterCustomer(filteredQueryForGraphQl);
+            // setCurrentPageToOne()
+            setFilteredQueryToGraphQl(filteredQueryForGraphQl)
+            console.log(filteredValueFromDataGrid(filter), 'filterValue');
+        } else {
+            setDefaultFilteredQueryToGraphQl()
+        }
+    }
+
+    const setCurrentPageToOne = () => {
+        onPageChangeCustomer(1)
+    }
+
+    // const
+
+    console.log(page, 'page');
 
     const intl = useIntl();
     const columns = [
@@ -97,7 +124,7 @@ export default function CustomerTable({ customers, onOrderCustomer, onPageChange
         onPageChange={handlePage}
         onPageSizeChange={handlePageSize}
         pageSize={limit}
-        rowsPerPageOptions={[2,4,6]}
+        rowsPerPageOptions={[2, 4, 6]}
         page={page - 1}
         // autoPageSize={true}
         rowCount={totalCustomer}
