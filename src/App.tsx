b938653737c2695ceda76951ecad6fda076c4ce6
Martin Svoren <msvoro@gmail.com>
2021-08-03 12:24:20 +0200

    add postHog

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,14 @@ import PropTypes from 'prop-types';
 import { IntlProvider } from 'react-intl';
 import 'src/mixins/chartjs';
 import theme from 'src/theme';
+import posthog from 'posthog-js';
 import routes from 'src/routes';
+
 
 const App = ({ locale, messages }) => {
   const routing = useRoutes(routes);
+  posthog.init("6Zx_zs97Kj_rTGcICZiauGWrZ4izzyMgnd2rATVT1_s", {api_host: 'https://app.posthog.com'});
+
   return (
     <IntlProvider locale={locale} messages={messages}>
       <ThemeProvider theme={theme}>
