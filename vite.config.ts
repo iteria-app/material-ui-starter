f442f6afd44b4f099db1af0c1690d71192a4f2e0
misosviso <56072239+misosviso@users.noreply.github.com>
2022-02-28 10:22:07 +0100

    Setting NODE_ENV to development in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,18 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import iteriaLowcode from '@iteria-app/vite-plugin-lowcode'
 
-process.env.CWD = process.cwd()
-const endpointURL = loadEnv(
-  process.env.NODE_ENV,
-  process.env.CWD
-)?.VITE_HASURA_GRAPHQL_ENDPOINT
+// process.env.CWD = process.cwd()
+// const endpointURL = loadEnv(
+//   process.env.NODE_ENV,
+//   process.env.CWD
+// )?.VITE_HASURA_GRAPHQL_ENDPOINT
 
-console.log(process.env.NODE_ENV)
 
-export default defineConfig({
+export default defineConfig(
+  {
   plugins: [react(),
   iteriaLowcode({ 
-    graphQLEndpoint: endpointURL,
+    //graphQLEndpoint: endpointURL,
     define: {
       "process.env.NODE_ENV": `"development"`,
     },
