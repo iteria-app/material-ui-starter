611467501963e5a41dd2e90ef20788c1173dc01d
Rasťo Balcerčík <rasto.balcercik@gmail.com>
2022-03-07 17:17:30 +0000

    feat: vite.confi.ts change hardcoded development mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,13 @@ import iteriaLowcode from './src/index'
 export default ({ command, mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()), VITE_CWD: process.cwd() }
 
+  console.log(command, mode)
+
   return defineConfig({
     plugins: [react(),
     iteriaLowcode({
       command,
-      mode,
+      mode: 'development',
       graphQLEndpoint: process.env.VITE_HASURA_GRAPHQL_ENDPOINT,
       cwd: process.cwd(),
       injectMode: Boolean(process.env.GITPOD_WORKSPACE_ID)
