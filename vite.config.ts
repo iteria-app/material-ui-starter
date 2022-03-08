f3dc622a2b7298bbc5d748724a5a7648c46ce7b7
Rasťo Balcerčík <rasto.balcercik@gmail.com>
2022-03-08 14:43:11 +0000

    feat: new version of vite-plugin-lowcode

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import iteriaLowcode from './src/index'
+import iteriaLowcode from '@iteria-app/vite-plugin-lowcode'
 
 export default ({ command, mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
