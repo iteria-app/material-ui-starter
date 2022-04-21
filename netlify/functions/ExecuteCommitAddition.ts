8414dadf8aa5aa46adefc317f04e5185958e7df0
misosviso <56072239+misosviso@users.noreply.github.com>
2022-04-21 11:38:59 +0200

    commit

--- netlify/functions/ExecuteCommitAddition.ts
+++ netlify/functions/ExecuteCommitAddition.ts
@@ -4,25 +4,24 @@ import NetlifyGraph, { CommitAdditionInput, NetlifyGraphFunctionOptions } from '
 
 export const handler = async function(event, context) {
 
-  console.log("Executing commit addition")
-
   const headOid = event.headers.headoid
   const commitMessage = event.headers.commitmessage
   const accessToken = event.authlifyToken
-
-  console.log("headOid: " + headOid)
-  console.log("commit message: " + commitMessage)
+  ///////////////////////////////////////////
+  const branchName = "build-branch"//event.headers.branchname
+  const repositoryNameWithOwner = "misosviso/example-material-ui"//event.headers.repositorynamewithowner
+  ///////////////////////////////////
+  const content = event.headers.content
+  const path = event.header.path
 
   const input: CommitAdditionInput = {
-    branchName: "main",
-    repositoryNameWithOwner: "misosviso/example-material-ui",
+    branchName: branchName,
+    repositoryNameWithOwner: repositoryNameWithOwner,
     expectedHeadOid: headOid,
     contents1: "YWJlY2VkYSB6amVkbGEgZGVkYQpwb3ZlZGFsYSBuYSBtZWR2ZWRhCmEgbWVkdmVkIGJvbCBvcGl0eQoKTmFrb3BhbCB0dSBiYWJrdSBqYWsgc2EgdnJhdmkuLi4gZG9yaWNpICg6ICg6",
     path: "bordel/bordel.txt",
     headline: commitMessage
   }
-
-  console.log(input);
 
   // @ts-ignore
   const { errors: ExecuteCommitErrors, data: ExecuteCommitData } = await NetlifyGraph.executeCommitAddition(input, { accessToken })
