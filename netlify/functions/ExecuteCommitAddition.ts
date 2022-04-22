7c7707ebcbdda670b16967a58fd3dc39cfef7401
misosviso <56072239+misosviso@users.noreply.github.com>
2022-04-22 16:40:33 +0200

    Final, hopefully

--- netlify/functions/ExecuteCommitAddition.ts
+++ netlify/functions/ExecuteCommitAddition.ts
@@ -11,7 +11,7 @@ export const handler = async function (event, context) {
   const branchName = 'build-brach'; //event.headers.branchname
   const repositoryNameWithOwner = 'misosviso/example-material-ui'; //event.headers.repositorynamewithowner
   const content = event.headers.content;
-  const path = event.headers.filePath;
+  const path = event.headers.filepath;
 
   console.log("Path", path)
   console.log("Content", content)
