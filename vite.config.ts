b46c300a50e52ac67ac394c68d4548ecf71362d8
misosviso <56072239+misosviso@users.noreply.github.com>
2022-04-22 10:36:05 +0200

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,75 +1,73 @@
-// import { defineConfig, loadEnv } from 'vite'
-// import react from '@vitejs/plugin-react'
-// //import iteriaLowcode from '@iteria-app/vite-plugin-lowcode'
-// import iteriaLowcode from './vite-plugin/node_modules/@iteria-app/vite-plugin-lowcode'
-// import * as path from 'path'
-
-
-// export default ({ command, mode }) => {
-//   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-
-
-//   return defineConfig({
-//     optimizeDeps: {
-//       exclude: ['@iteria-app/wysiwyg'] //, '@iteria-app/component-templates', '@iteria-app/ide-devserver', '@iteria-app/generator', '@iteria-app/graphql-lowcode']
-//     },
-//     resolve: {
-//       alias: {
-//         os: 'os-browserify',
-//         path: 'path-browserify',
-//         module: path.resolve(__dirname, './src/constants.ts'),
-//       },
-//     },
-//     server: {
-//       port: 3000,
-//     },
-//     plugins: [
-//       react(),
-//       iteriaLowcode({
-//         command,
-//         mode,
-//         graphQLEndpoint: process.env.VITE_HASURA_GRAPHQL_ENDPOINT,
-//         cwd: process.cwd(),
-//         version: "workspace:*",
-//         // injectMode: Boolean(process.env.GITPOD_WORKSPACE_ID)
-//         //   ? 'devServer'
-//         //   : 'jamstack',
-//       }),
-//     ],  
-//     define: {
-//       "process.env.NODE_ENV": `"development"`,
-//     },
-//     build: {
-//       sourcemap: false
-//     }
-//   })
-// }
-
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-// import iteriaLowcode from '@iteria-app/vite-plugin-lowcode'
+//import iteriaLowcode from '@iteria-app/vite-plugin-lowcode'
 import iteriaLowcode from './vite-plugin'
-
-process.env.CWD = process.cwd()
-process.env.VITE_CWD = process.cwd()
-
-const endpointURL = loadEnv(
-  process.env.NODE_ENV ?? 'development',
-  process.env.CWD
-)?.VITE_HASURA_GRAPHQL_ENDPOINT
+import * as path from 'path'
 
 
-export default defineConfig(
-  {
-  plugins: [react(),
-  iteriaLowcode({ 
-    graphQLEndpoint: endpointURL,
-    //injectDevServer: true
-  })],
-  define: {
-    "process.env.NODE_ENV": `"development"`,
-  },
-})
+export default ({ command, mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+
+  return defineConfig({
+    optimizeDeps: {
+      exclude: ['@iteria-app/wysiwyg'] //, '@iteria-app/component-templates', '@iteria-app/ide-devserver', '@iteria-app/generator', '@iteria-app/graphql-lowcode']
+    },
+    resolve: {
+      alias: {
+        os: 'os-browserify',
+        path: 'path-browserify',
+        module: path.resolve(__dirname, './src/constants.ts'),
+      },
+    },
+    server: {
+      port: 3000,
+    },
+    publicDir: "/public",
+    plugins: [
+      react(),
+      iteriaLowcode({
+        command,
+        mode,
+        graphQLEndpoint: process.env.VITE_HASURA_GRAPHQL_ENDPOINT,
+        cwd: process.cwd(),
+        // version: "workspace:*",
+        injectMode: Boolean(process.env.GITPOD_WORKSPACE_ID)
+          ? 'devServer'
+          : 'jamstack',
+      }),
+    ],  
+    define: {
+      "process.env.NODE_ENV": `"development"`,
+    },
+  })
+}
+
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+// // import iteriaLowcode from '@iteria-app/vite-plugin-lowcode'
+// import iteriaLowcode from './vite-plugin'
+
+// process.env.CWD = process.cwd()
+// process.env.VITE_CWD = process.cwd()
+
+// const endpointURL = loadEnv(
+//   process.env.NODE_ENV ?? 'development',
+//   process.env.CWD
+// )?.VITE_HASURA_GRAPHQL_ENDPOINT
+
+
+// export default defineConfig(
+//   {
+//   plugins: [react(),
+//   iteriaLowcode({ 
+//     graphQLEndpoint: endpointURL,
+//     //injectDevServer: true
+//   })],
+//   define: {
+//     "process.env.NODE_ENV": `"development"`,
+//   },
+// })
 
 
 
