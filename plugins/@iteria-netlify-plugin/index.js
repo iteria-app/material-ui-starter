1b937c83c5e0403c8eb437285af5b2641be8334c
MatusVetrik1 <matus.vetrik@iteria.io>
2022-04-25 10:41:42 +0200

    default tar

--- plugins/@iteria-netlify-plugin/index.js
+++ plugins/@iteria-netlify-plugin/index.js
@@ -4,20 +4,20 @@ const { modifyPlugins } = require('./modifyPlugins');
 
 module.exports = {
   onPreBuild: async ({ utils: { run } }) => {
-    await run.command('mkdir ./temp_archive');
+    // await run.command('mkdir ./temp_archive');
     await run.command(
-      'tar --exclude temp_archive --exclude node_modules --exclude .git --exclude public --exclude plugins -cf ./temp_archive/Sources.tar ./'
+      'tar --exclude node_modules --exclude dist --exclude public  -cvzf ./public/Sources.tar.gz ./'
     );
     // tar --exclude node_modules --exclude dist --exclude public  -cvzf ./public/Sources.tar.gz ./
 
     generateIndex();
     //addScriptsToIndex()
     //modifyPlugins()
-  },
-  onBuild: async ({ utils: { run } }) => {
-    await run.command('tar -rf ./temp_archive/Sources.tar '); //./src/generated")
-    await run.command('gzip -9 ./temp_archive/Sources.tar');
-    await run.command('mv ./temp_archive/Sources.tar.gz ./build');
-    await run.command('rm -rf ./temp_archive');
   }
+  // onBuild: async ({ utils: { run } }) => {
+  //   await run.command('tar -rf ./temp_archive/Sources.tar '); //./src/generated")
+  //   await run.command('gzip -9 ./temp_archive/Sources.tar');
+  //   await run.command('mv ./temp_archive/Sources.tar.gz ./build');
+  //   await run.command('rm -rf ./temp_archive');
+  // }
 };
