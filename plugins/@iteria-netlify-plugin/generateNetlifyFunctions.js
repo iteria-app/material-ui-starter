ab85ad99c3f9453a9ca09bc682d6fce70bec8a43
PatrikOndrus <patrik.ondrus@iteria.io>
2022-05-03 13:46:27 +0200

    headoid

--- plugins/@iteria-netlify-plugin/generateNetlifyFunctions.js
+++ plugins/@iteria-netlify-plugin/generateNetlifyFunctions.js
@@ -70,15 +70,23 @@ const getFetchHeadOid = `import NetlifyGraph from './netlifyGraph'
 
 export const handler = async function(event, context) {
 
-  console.log(event.netlifyGraphToken)
-  
-  const {errors, data} = await NetlifyGraph.fetchFetchHeadOid({/* variables */}, {accessToken: event.netlifyGraphToken})
+  console.log(event.netlifyGraphToken);
+
+  const { errors, data } = await NetlifyGraph.fetchFetchHeadOid(
+    {
+      /* variables */
+    },
+    { accessToken: event.netlifyGraphToken }
+  );
+
+  console.log(errors);
+  console.log(data);
 
   return {
     statusCode: errors ? 500 : 200,
-    body: JSON.stringify(errors || data),
-    headers: {"Content-Type": "application/json"}
-  }
+    body: { errors, data },
+    headers: { 'Content-Type': 'application/json' }
+  };
 }`;
 
 const indexJs = `// GENERATED VIA NETLIFY AUTOMATED DEV TOOLS, EDIT WITH CAUTION!
