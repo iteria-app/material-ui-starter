a1c08ff76d409e70be1b420b7f744e58c44abd45
Patrik Ondruš <102805075+PatrikOndrus@users.noreply.github.com>
2022-05-04 12:42:05 +0200

    netlify commit

--- src/layouts/DashboardLayout/index.tsx
+++ src/layouts/DashboardLayout/index.tsx
@@ -8,9 +8,9 @@ const useStyles = makeStyles((theme: any) => ({
   root: {
     backgroundColor: theme.palette.background.dark,
     display: 'flex',
-    height: '10%',
+    height: '100%',
     overflow: 'hidden',
-    width: '10%'
+    width: '100%'
   },
   wrapper: {
     display: 'flex',
