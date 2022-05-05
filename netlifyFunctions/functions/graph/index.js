562bef0b230e08efd962d519933d6a7f8dc7935b
PatrikOndrus <patrik.ondrus@iteria.io>
2022-05-05 12:51:00 +0200

    test 3

--- netlifyFunctions/functions/graph/index.js
+++ netlifyFunctions/functions/graph/index.js
@@ -51,11 +51,7 @@ exports.verifySignature = (input) => {
 };
 
 const operationsDoc = `
-input AdditionsInput {
-  contents: GitHubBase64String = ""
-  path: String = ""
-}
-mutation CommitAddition($contents: GitHubBase64String = "", $branchName: String = "", $repositoryNameWithOwner: String = "", $clientMutationId: String = "", $headline: String = "", $additions: [AdditionsInput], $expectedHeadOid: GitHubGitObjectID = null) @netlify(id: """2c9d16fa-b843-48a6-85df-8c3aca9d1882""", doc: """An empty mutation to start from""") {
+mutation CommitAddition($contents: GitHubBase64String = "", $branchName: String = "", $repositoryNameWithOwner: String = "", $clientMutationId: String = "", $headline: String = "", $additions: [ {contents1: GitHubBase64String = "",   path: String = ""}], $expectedHeadOid: GitHubGitObjectID = null) @netlify(id: """2c9d16fa-b843-48a6-85df-8c3aca9d1882""", doc: """An empty mutation to start from""") {
   gitHub {
     createCommitOnBranch(
       input: {
