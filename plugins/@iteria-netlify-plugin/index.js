c26e74c8a7590aa23c689468616d84f51ea50beb
Patrik Ondruš <102805075+PatrikOndrus@users.noreply.github.com>
2022-05-20 12:49:34 +0200

    l

--- plugins/@iteria-netlify-plugin/index.js
+++ plugins/@iteria-netlify-plugin/index.js
@@ -1,18 +1,19 @@
-const { generateIndex } = require('./generateIndex');
+const { generateIndex } = require('./generateIndex')
 const { generateNetlifyFunctions } = require('./generateNetlifyFunctions')
-const { addScriptsToIndex } = require('./addScripts');
-const { modifyPlugins } = require('./modifyPlugins');
+const { addScriptsToIndex } = require('./addScripts')
+const { modifyPlugins } = require('./modifyPlugins')
 
 module.exports = {
   onPreBuild: async ({ utils: { run } }) => {
-    await run.command('mkdir ./temp_archive');
+    await run.command('mkdir ./temp_archive')
     await run.command(
       'tar --exclude node_modules --exclude dist --exclude public --exclude netlify --exclude plugins --exclude vite-plugin --exclude .git --exclude .prettierignore --exclude .vscode  -cvzf ./public/Sources.tar.gz ./'
-    );
+    )
 
+    console.log(process.env.SITE_ID)
     console.log(process.env.BRANCH)
-    generateIndex();
-    generateNetlifyFunctions();
+    generateIndex()
+    generateNetlifyFunctions()
     //addScriptsToIndex()
     //modifyPlugins()
   }
@@ -23,4 +24,4 @@ module.exports = {
   //   await run.command('mv ./temp_archive/Sources.tar.gz ./build');
   //   await run.command('rm -rf ./temp_archive');
   // }
-};
+}
