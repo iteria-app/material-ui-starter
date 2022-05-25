09fbd9452d52f906bf2f4e6e141f9d6e891a01d3
rastiQ <rasto.balcercik@gmail.com>
2022-05-25 17:22:41 +0200

    feat: react-refresh dep

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import { LocaleContextProvider } from '@iteria-app/component-templates/src/i18n'
 
-// @ts-ignore
-if (!window.__skip_render) {
-  ReactDOM.render(
-    <LocaleContextProvider>
-      <Router>
-        <App />
-      </Router>
-    </LocaleContextProvider>,
-    document.getElementById('root')
-  )
-}
+ReactDOM.render(
+  <LocaleContextProvider>
+    <Router>
+      <App />
+    </Router>
+  </LocaleContextProvider>,
+  document.getElementById('root')
+)
