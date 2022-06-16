dec4cf44b00b7b4fc35ad7117ec84ebcd5415723
rastiQ <rasto.balcercik@gmail.com>
2022-06-16 17:12:01 +0200

    feat: test plugin changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import iteriaLowcode from './plugin'
+import iteriaLowcode from '@rastiq/vite-plugin-lowcode'
 import path from 'path'
 
 export default ({ command, mode }) => {
