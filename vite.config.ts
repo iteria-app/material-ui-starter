c4eb5811843de99a3dc22dc485967b7bc3d002f7
hanzeltomas <hanzel.toma@gmail.com>
2022-06-16 17:52:45 +0200

    feat: loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import iteriaLowcode from './index'
+import iteriaLowcode from './index.js'
 import path from 'path'
 
 export default ({ command, mode }) => {
