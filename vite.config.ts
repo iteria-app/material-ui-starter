f74b86a826947e0483860cb6da424982a9f0b92c
rastiQ <rasto.balcercik@gmail.com>
2022-07-20 14:14:26 +0200

    feat: test injectLowcode false

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default ({ command, mode }) => {
         graphQLEndpoint: env.VITE_HASURA_GRAPHQL_ENDPOINT,
         graphQLSecret: env.VITE_HASURA_GRAPHQL_SECRET,
         cwd: process.cwd(),
-        injectLowcode: true,
+        injectLowcode: false,
         features: {
           tippy: true,
           generator: true,
